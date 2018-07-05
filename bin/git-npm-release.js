d6e52e7bc57580ff8172a807f3359dd56e3ffedc
Patrick Force <patrickforce@gmail.com>
2018-07-05 09:17:54 -0600

    fix argument position

--- bin/git-npm-release.js
+++ bin/git-npm-release.js
@@ -5,5 +5,5 @@ const path        = require('path')
 const fs          = require('fs')
 const Releaser    = require('../lib/releaser').default
 const r           = new Releaser()
-const permission  = process.argv.length > 1 ? process.argv[1] : 'public'
+const permission  = process.argv.length > 2 ? process.argv[2] : 'public'
 r.release(permission)
